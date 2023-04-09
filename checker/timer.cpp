96936ae04a01390822edc6af02fe77e86c21ae86
Alexey Stepanov <st3p-by-step@ya.ru>
2023-04-09 21:12:44 +0300

    Now timer does 100 iterations per an algorithm

--- checker/timer.cpp
+++ checker/timer.cpp
@@ -9,8 +9,8 @@ std::vector<std::vector<int64_t>> timer(std::vector<int> (*func)(const std::stri
     int upper_border = input_text.size() - MAX_SUBSTR_SIZE;
     std::vector<std::vector<int64_t>> times(MAX_SUBSTR_SIZE / STEP, std::vector<int64_t>(10));
     for (int n = STEP; n <= MAX_SUBSTR_SIZE; n += STEP) {
-        std::vector<int64_t> time(10);
-        for (int i = 0; i < 10; i++) {
+        std::vector<int64_t> time(100);
+        for (size_t i = 0; i < time.size(); i++) {
             srand(std::time(nullptr));
             std::string sub = input_text.substr(rand() % upper_border, n);
             auto start = std::chrono::high_resolution_clock::now();
