54bfb36baccbc61835f060e4cfb01ebe52659ca4
Alexey Stepanov <st3p-by-step@ya.ru>
2023-04-09 20:29:39 +0300

    Regex update in runComparisonsToFile() and runTimeToFile()

--- task_execution/time_to_file.cpp
+++ task_execution/time_to_file.cpp
@@ -7,7 +7,7 @@
 void runTimeToFile(
     std::vector<std::vector<int> (*)(const std::string &, const std::string &)> functions,
     std::vector<std::string> names, std::vector<std::string> pathes, std::fstream &file) {
-    std::regex pattern("/([^/]+)\\.txt$");
+    std::regex pattern("([^/]+)(?=\\.txt$)");
     for (size_t i = 0; i < functions.size(); ++i) {
         for (size_t j = 0; j < pathes.size(); ++j) {
             std::smatch match;
